3c824eb648da35e93688b507353172cb1e30a4fb
Tiziano Iacovelli <tiacovel@c4b7c2.42berlin.de>
2023-11-16 18:01:07 +0100

    modify function strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -18,15 +18,15 @@ char *ft_strrchr(const char *str, int c)
 		return (char *)str;
 	pos = 0;
 	while (*str != '\0')
-		{
-			if (*str == c)
-				pos = (char *)str;
-			str++;
-		}
+	{
+		if (*str == c)
+			pos = (char *)str;
+		str++;
+	}
 	return (char *)pos;
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
 #include <string.h>
 int	main(void)
 {
@@ -36,5 +36,4 @@ int	main(void)
 	printf("42 function: %s\n", ft_strrchr(string, c));
 	printf("STD function: %s", strrchr(string, c));
 	return (0);
-}
-
+} */
