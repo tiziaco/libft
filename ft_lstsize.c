dc6f422b87fd755a4d50a7de5c1dbc20fd4bb3df
tiziaco <tiziano.iaco@gmail.com>
2023-11-17 20:53:41 +0100

    add list functions

---
+++ ft_lstsize.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 18:20:15 by tiacovel          #+#    #+#             */
+/*   Updated: 2023/11/17 18:25:31 by tiacovel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "libft.h"
+
+int ft_lstsize(t_list *lst)
+{
+	int	size;
+	t_list	*ptr;
+
+	size = 0;
+	if (!lst)
+		return (size);
+	ptr = lst;
+	while (!ptr)
+	{
+		size++;
+		ptr = ptr->next;
+	}
+	return (size);
+}
