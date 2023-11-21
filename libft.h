2dd332904320314704c91d58f41bb753f932240b
tiziaco <tiziano.iaco@gmail.com>
2023-11-21 13:12:12 +0100

    modify header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:56:52 by tiacovel          #+#    #+#             */
-/*   Updated: 2023/11/20 17:30:14 by tiacovel         ###   ########.fr       */
+/*   Updated: 2023/11/21 12:31:59 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,10 +64,10 @@ int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
 void			ft_lstadd_back(t_list **lst, t_list *new);
 void			ft_lstadd_front(t_list **lst, t_list *new);
-//void			ft_lstdelone(t_list *lst, void (*del)(void *));
-//void			ft_lstclear(t_list **lst, void (*del)(void *));
-//void			ft_lstiter(t_list *lst, void (*f)(void *));
-//t_list		*ft_lstmap(t_list *lst, void *(*f)(void *),
-//					void (*del)(void *));
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstclear(t_list **lst, void (*del)(void *));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 
 #endif
